4f695142a2a841da2555a7800c5a038110e85718
Wes Campaigne <westacular@gmail.com>
2013-03-26 16:27:59 -0400

    Fixes errant newlines being added or removed when the selected region contains multiple leading or trailing newlines.

--- Code.popclipext/code.rb
+++ Code.popclipext/code.rb
@@ -2,14 +2,25 @@
 
 input = ENV['POPCLIP_TEXT']
 
-if input.split("\n").length > 1
+lines = input.split("\n", -1)
+if lines.length > 1
+	if lines[-1].empty?
+		lines.pop
+		trailingnewline = true
+	else
+		trailingnewline = false
+	end
 	output = ""
-	input.split("\n").each {|line|
+	lines.each {|line|
 	  output += "\t#{line}\n"
 	}
-	print output.sub(/\n$/s,'')
+	if trailingnewline
+		print output
+	else
+		print output.sub(/\n\z/s,'')
+	end
 else
-	head = input =~ /^(\s+)/ ? $1 : ''
-	tail = input =~ /(\s+)$/ ? $1 : ''
+	head = input =~ /\A(\s+)/ ? $1 : ''
+	tail = input =~ /(\s+)\z/ ? $1 : ''
 	print "#{head}`#{input.strip}`#{tail}"
 end
